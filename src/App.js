b65968943e039fe37f60ecc13b6f71aebf2e3ed5
John Leonardo <jdleonardo5@icloud.com>
2018-04-05 10:04:41 -0700

    brought in mdl layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//MDL components needed
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Textfield from 'react-mdl/lib/Textfield';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="demo-big-content">
+        <Layout>
+            <Header title="Title" scroll>
+                <Navigation>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content>
+                <div className="page-content" />
+            </Content>
+        </Layout>
       </div>
     );
   }
